08de4ee736aa054f1c730180b61ff41c8040c3ae
Sjoer van der Ploeg <sfjuocekr@gmail.com>
2022-01-18 20:04:23 +0100

    Update config.h
    Removed some "private" stuff nobody has a use for.

--- src/config.h
+++ src/config.h
@@ -1,7 +1,7 @@
 #define LOGPERIOD 1000
 
-const char *ssid = "Puttyboy";
-const char *password = "fawakaswa37";
-const char *mqtt_server = "10.0.0.3";
+const char *ssid = "YOUR_SSID";
+const char *password = "YOUR_PSK";
+const char *mqtt_server = "MQTT_HOST";
 const unsigned int mqtt_port = 1883;
-const char *time_server = "nl.pool.ntp.org";
+const char *time_server = "pool.ntp.org";
